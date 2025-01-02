065b9cf8710c36a467dfbdffadc52cc2d1ce21a5
Abdulaziz20007 <lil.gangsser@gmail.com>
2025-01-02 10:49:39 +0500

    ok

--- app.js
+++ app.js
@@ -5,7 +5,21 @@ const cookieParser = require("cookie-parser");
 const routes = require("./routes");
 
 const app = express();
-const PORT = config.get("port") || 3030;
+const PORT = config.get("port") || 4411;
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.use(express.json());
 app.use(cookieParser());
